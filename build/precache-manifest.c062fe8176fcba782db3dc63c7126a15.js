5e32ac9abd3edbaba23d8203ba7e1d013c4c69df
Vadim Terletsky <vadim.terletsky@da-14.com>
2020-04-17 19:44:07 +0300

    Add build folder and server script

---
+++ build/precache-manifest.c062fe8176fcba782db3dc63c7126a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff6814e7eb64a578b7d77407460a013",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce5e11d2daea12419891",
+    "url": "/static/css/main.412f37e3.chunk.css"
+  },
+  {
+    "revision": "05dbd2107d9cd2f648a7",
+    "url": "/static/js/2.8844cc73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8844cc73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5e11d2daea12419891",
+    "url": "/static/js/main.a1b5ef44.chunk.js"
+  },
+  {
+    "revision": "ec71a6f454779efffb4b",
+    "url": "/static/js/runtime-main.af1b6dae.js"
+  }
+]);
